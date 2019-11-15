4cd7711b547391946b2b6b569a4dc84c1c96d1a3
shailen <shailen@lumkani.com>
2019-11-15 19:23:30 +0200

    Updated docs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  description: 'A nice way of handling global events in Vue components'
+  description: 'A nice way of handling global events in Vue components',
+  themeConfig: {
+    repo: 'ShailenNaidoo/vuevent'
+  }
 }
